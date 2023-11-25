0435bb392c553664f8b7a223e0797ede9ad9ec04
aliniluca <115418094+aliniluca@users.noreply.github.com>
2023-11-25 15:11:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,17 +8,18 @@ import personRouter from "./src/routes/personRoutes.js";
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV ?? 'development'}` })
 const app = express();
-
+// Allow requests from your client domain
+app.use(cors({ origin: 'https://horoscope-w6rd.onrender.com/' }));
 app.use(express.json());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+
 
 // Add Routes
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/person", personRouter);
 app.use("/", (req, res)=>{
-  res.status(200).send(`<h1>AstroAI backend working successfully in ${process.env.NODE_ENV} environment</h1>`)
+  res.status(200).send(`<h1> Backend working successfully in ${process.env.NODE_ENV} environment</h1>`)
 });
 
 const port = process.env.PORT || 4000;
